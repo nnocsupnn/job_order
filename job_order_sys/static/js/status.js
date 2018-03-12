976ee3643b54d88ace4ef8c3415edfe26cf572dc
onincas <nincas21@gmail.com>
2018-03-12 16:07:06 +0800

    invoice and OR

--- job_order_sys/static/js/status.js
+++ job_order_sys/static/js/status.js
@@ -1,463 +1,70 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    {% load staticfiles %}
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.13/semantic.min.css">
-    <link rel="shortcut icon" href="{% static 'tabico/icon_doc.ico' %}">
-    <link rel="stylesheet" href="{% static 'style.css' %}">
-    
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-    <title>Projects - Mega A</title>
-</head>
-<body>
-        <div class="ui menu">
-            <div class="header item">
-                <a href="/megaa"><i class="ui folder open icon"></i> JOB Orders</a> 
-            </div>
-        </div>
+let invoicee = 0;
+let count__or = 0;
+$('#dataTable tr#dataa').each(function(){
+    let or = $(this).closest('tr').find('#joOR').val();
+    let coddd = $(this).closest('tr').find('#idCode').text();
 
-{% csrf_token %}
-<div class="ui blue raised segment" id="detailss" style="">
-        <h2 class="ui horizontal divider header">{{jos.jo_code}}</h2>
-        <a href="/megaa" class="ui circular facebook button"><i class="chevron left icon"></i></a>
-        <br>
-        <h2>{{ request.session.user }}</h2>
-    <div class="ui form">
-    <h4 class="ui dividing header">Project Information</h4>
-    <div class="field">
-    <div class="two fields">
-    <div class="field">
-        <label>Date</label>
-        <input id="jodate" type="text" value="{{jos.jo_date}}" class="datepicker">
-    </div>
-    <div class="field">
-        <label>Study</label>
-        <input id="jodate" type="text" value="{{jos.study}}" class="datepicker">
-    </div>
-    </div>
-    </div>
+    let inv1 = $(this).closest('tr').find('#inv_1').val();
+    let inv2 = $(this).closest('tr').find('#inv_2').val();
+    let inv3 = $(this).closest('tr').find('#inv_3').val();
+    let inv4 = $(this).closest('tr').find('#inv_4').val();
+    let inv5 = $(this).closest('tr').find('#inv_5').val();
+    let inv6 = $(this).closest('tr').find('#inv_6').val();
+    let inv7 = $(this).closest('tr').find('#inv_7').val();
+    let inv8 = $(this).closest('tr').find('#inv_8').val();
+    let inv9 = $(this).closest('tr').find('#inv_9').val();
+    let inv10 = $(this).closest('tr').find('#inv_10').val();
+    let inv11 = $(this).closest('tr').find('#inv_11').val();
+    let inv12 = $(this).closest('tr').find('#inv_12').val();
+    let inv13 = $(this).closest('tr').find('#inv_13').val();
 
-   <div class="field">
-    <div class="two fields">
-    <div class="field">
-         <label>Jo No.</label>
-        <input type="text" value="{{jos.jo_no}}">
-    </div>
-    <div class="field">
-         <label>JO Code</label>
-        <input type="text" value="{{jos.jo_code}}">
-    </div>
-    </div>
-    </div>
+    let or1 = $(this).closest('tr').find('#or_1').val();
+    let or2 = $(this).closest('tr').find('#or_2').val();
+    let or3 = $(this).closest('tr').find('#or_3').val();
+    let or4 = $(this).closest('tr').find('#or_4').val();
+    let or5 = $(this).closest('tr').find('#or_5').val();
+    let or6 = $(this).closest('tr').find('#or_6').val();
+    let or7 = $(this).closest('tr').find('#or_7').val();
+    let or8 = $(this).closest('tr').find('#or_8').val();
+    let or9 = $(this).closest('tr').find('#or_9').val();
+    let or10 = $(this).closest('tr').find('#or_10').val();
+    let or11 = $(this).closest('tr').find('#or_11').val();
+    let or12 = $(this).closest('tr').find('#or_12').val();
+    let or13 = $(this).closest('tr').find('#or_13').val();
 
-    <div class="field">
-    <div class="two fields">
-    <div class="field">
-         <label>Client</label>
-        <input type="text" value="{{jos.jo_clients}}">
-    </div>
-    <div class="field">
-        <label>QL / QN</label>
-        <input type="text" value="{{jos.jo_qlqnChoices}}">
-    </div>
-    </div>
-    </div>
+    //creates and array to store all data in single array var
+    let invAll = [inv1,inv2,inv3,inv4,inv5,inv6,inv7,inv8,inv9,inv10,inv11,inv12,inv13];
+    let orAll =  [or1,or2,or3,or4,or5,or6,or7,or8,or9,or10,or11,or12,or13];
 
-    <div class="field">
-    <div class="two fields">
-    <div class="field">
-        <label>Project No.</label>
-        <input id="projectno" type="text" value="{{jos.jo_project_no}}">
-    </div>
-    <div class="field">
-        <label>Contract Price</label>
-        <input id="contactprice" type="text" value="{{jos.jo_contact_price}}">
-    </div>
-    </div>
-    </div>
+    //loop for invoice array
+    for (var i = 0;i <= 12; i++) {
+    	if(invAll[i] == 0){
+    		
+    	}
+    	else {
+    		invoicee++;
+    	}
+    }
 
-    <div class="field">
-    <div class="two fields">
-    <div class="field">
-        <label>Terms</label>
-        <input type="text" value="{{jos.jo_terms}} days">
-    </div>
-    <div class="field">
-        <label>VAT</label>
-        <input type="text" value="{{jos.jo_vat}}">
-    </div>
-    </div>
-    </div>
+    //loop for or array
+    for (var i = 0;i <= 12; i++) {
+    	if(orAll[i] == 0){
+    		
+    	}
+    	else {
+    		count__or = count__or + 1;
+    	}
+    }
 
-    <div class="field">
-    <div class="two fields">
-    <div class="field">
-        <label id="">PO No.</label>
-        <input id="" type="text" value="{{jos.jo_po_no}}">
-        
-    </div>    
-    <div class="field">
-        
-        <label id="scLabel">Sign Contract</label>
-        <input id="SignContractStatus" type="text" value="{{jos.jo_sign_contract}}">
-    </div>
-    </div>
-    </div>
+    if (invoicee == count__or) {
+    	$(this).closest('tr').find('#invoiceNo').append('<br><i class=\"info circle green icon\"></i><b>PAID</b>');
+    }
+    else{
+    	$(this).closest('tr').find('#invoiceNo').append('<br><i class=\"info circle red icon\"></i><b>UNPAID</b>');
+    }
+	console.log(invoicee + '/' + count__or);
+    console.log('-----------------------------');
+    count__or = 0;
+    invoicee  = 0;
+});
 
-    <div class="field">
-    <div class="two fields">
-    <div class="field">
-        <label>Invoice No.</label>
-        <input type="text" value="{{jos.jo_inv_no}}">
-        
-    </div>
-    <div class="field">
-        <label>OR No.</label>
-        <input type="text" value="{{jos.jo_or}}">
-       
-    </div>
-    </div>
-    </div>
-
-    <div class="field">
-    <div class="two fields">
-    <div class="field">
-            <label>OR Date</label>
-            <input type="text" value="{{jos.jo_or_date}}">
-            
-    </div>
-    <div class="field">
-            <label>Second Invoice</label>
-            <input type="text" value="{{jos.jo_second_inv}}">
-       
-     </div>
-    </div>
-    </div>
-
-    <!-- all invoices -->
-    <div class="field">
-    <div class="two fields">
-    <div class="field">
-            <label>Invoice 1</label>
-            <input type="text" value="{{jos.jo_invoice1}}">
-            
-    </div>
-    <div class="field">
-            <label>OR 1</label>
-            <input type="text" value="{{jos.jo_or1}}">
-       
-     </div>
-    </div>
-    </div>.<div class="field">
-    <div class="two fields">
-    <div class="field">
-            <label>Invoice 2</label>
-            <input type="text" value="{{jos.jo_invoice2}}">
-            
-    </div>
-    <div class="field">
-            <label>OR 2</label>
-            <input type="text" value="{{jos.jo_or2}}">
-       
-     </div>
-    </div>
-    </div>
-    <div class="field">
-    <div class="two fields">
-    <div class="field">
-            <label>Invoice 3</label>
-            <input type="text" value="{{jos.jo_invoice3}}">
-            
-    </div>
-    <div class="field">
-            <label>OR 3</label>
-            <input type="text" value="{{jos.jo_or3}}">
-       
-     </div>
-    </div>
-    </div>
-    <div class="field">
-    <div class="two fields">
-    <div class="field">
-            <label>Invoice 4</label>
-            <input type="text" value="{{jos.jo_invoice4}}">
-            
-    </div>
-    <div class="field">
-            <label>OR 4</label>
-            <input type="text" value="{{jos.jo_or4}}">
-       
-     </div>
-    </div>
-    </div>
-    <div class="field">
-    <div class="two fields">
-    <div class="field">
-            <label>Invoice 5</label>
-            <input type="text" value="{{jos.jo_invoice5}}">
-            
-    </div>
-    <div class="field">
-            <label>OR 5</label>
-            <input type="text" value="{{jos.jo_or5}}">
-       
-     </div>
-    </div>
-    </div>
-    <div class="field">
-    <div class="two fields">
-    <div class="field">
-            <label>Invoice 6</label>
-            <input type="text" value="{{jos.jo_invoice6}}">
-            
-    </div>
-    <div class="field">
-            <label>OR 6</label>
-            <input type="text" value="{{jos.jo_or6}}">
-       
-     </div>
-    </div>
-    </div>
-    <div class="field">
-    <div class="two fields">
-    <div class="field">
-            <label>Invoice 7</label>
-            <input type="text" value="{{jos.jo_invoice7}}">
-            
-    </div>
-    <div class="field">
-            <label>OR 7</label>
-            <input type="text" value="{{jos.jo_or7}}">
-       
-     </div>
-
-    </div>
-    </div>
-     <div class="field">
-    <div class="two fields">
-    <div class="field">
-            <label>Invoice 8</label>
-            <input type="text" value="{{jos.jo_invoice8}}">
-            
-    </div>
-    <div class="field">
-            <label>OR 8</label>
-            <input type="text" value="{{jos.jo_or8}}">
-       
-     </div>
-     
-    </div>
-    </div>
-     <div class="field">
-    <div class="two fields">
-    <div class="field">
-            <label>Invoice 9</label>
-            <input type="text" value="{{jos.jo_invoice9}}">
-            
-    </div>
-    <div class="field">
-            <label>OR 9</label>
-            <input type="text" value="{{jos.jo_or9}}">
-       
-     </div>
-     
-    </div>
-    </div>
-     <div class="field">
-    <div class="two fields">
-    <div class="field">
-            <label>Invoice 10</label>
-            <input type="text" value="{{jos.jo_invoice10}}">
-            
-    </div>
-    <div class="field">
-            <label>OR 10</label>
-            <input type="text" value="{{jos.jo_or10}}">
-       
-     </div>
-     
-    </div>
-    </div>
-     <div class="field">
-    <div class="two fields">
-    <div class="field">
-            <label>Invoice 11</label>
-            <input type="text" value="{{jos.jo_invoice11}}">
-            
-    </div>
-    <div class="field">
-            <label>OR 11</label>
-            <input type="text" value="{{jos.jo_or11}}">
-       
-     </div>
-     
-    </div>
-    </div>
-     <div class="field">
-    <div class="two fields">
-    <div class="field">
-            <label>Invoice 12</label>
-            <input type="text" value="{{jos.jo_invoice12}}">
-            
-    </div>
-    <div class="field">
-            <label>OR 12</label>
-            <input type="text" value="{{jos.jo_or12}}">
-       
-     </div>
-     
-    </div>
-    </div>
-     <div class="field">
-    <div class="two fields">
-    <div class="field">
-            <label>Invoice 13</label>
-            <input type="text" value="{{jos.jo_invoice13}}">
-            
-    </div>
-    <div class="field">
-            <label>OR 13</label>
-            <input type="text" value="{{jos.jo_or13}}">
-       
-     </div>
-     
-    </div>
-    </div>
-     <div class="field">
-    <div class="two fields">
-    <div class="field">
-            <label>OR Date</label>
-            <input type="text" value="{{jos.jo_or_date}}">
-            
-    </div>
-    <div class="field">
-            <label>Second Invoice</label>
-            <input type="text" value="{{jos.jo_second_inv}}">
-       
-     </div>
-     
-    </div>
-    </div>
-    <!--- end of invoices -->
-
-    <div class="field">
-    <div class="two fields">
-    <div class="field">
-        
-            <label>Payment Date 1</label>
-            <input type="text" value="{{jos.jo_payment_date1}}">
-       
-           
-    </div>
-    <div class="field">
-            <label>Addendum</label>
-            <input type="text" value="{{jos.jo_addendum}}">
-        
-    </div>
-    </div>
-    </div>
-    <div class="field">
-    <div class="two fields">
-    <div class="field">
-        
-            <label>Final Billing</label>
-            <input type="text" value="{{jos.jo_addendum}}">
-  
-    </div>
-    <div class="field">
-            <label>Payment Date 2</label>
-            <input type="text" value="{{jos.jo_payment_date2}}">
-    </div>
-    </div>
-
-    
-            <label>Remarks</label>
-            <textarea type="text" value="">{{jos.jo_remarks}}</textarea>
-  
-    </div>
-    
-    
-    {% load staticfiles %}
-    <div class="ui black raised segment">
-    <h3 class="ui horizontal divider header">Leave a Comment</h3>
-    <div class="ui info message" id="status"></div>
-    <div class="ui comments" id="">
-            <div class="comment" id="commentDiv">  
-            </div>
-        {% for commentss in commentss%}
-            <div class="comment" id="commentDiv">
-              <a class="avatar">
-                <img class="ui circular image" src="{% static 'tabico/user.png' %}">
-              </a>
-              <div class="content">
-                <a class="author">{{commentss.comment_user}}</a>
-                <div class="metadata">
-                  <div class="date">{{commentss.comment_date}}</div>
-                </div>
-                
-                <div class="text" id="commentText">
-                    {{commentss.comment_text}}
-                </div>
-              </div>
-            </div>
-            {% endfor %}
-            <br><br>
-            <hr>
-            <div class="field">
-              <div class="field">
-                <label>Your Comment</label>
-                <textarea id="comment2" placeholder="Enter your comment here.."></textarea>
-              </div>
-              <div id="submitComment" class="ui positive submit labeled icon button">
-                <i class="icon edit"></i> Add Comment
-              </div>
-          </div>
-        </div>
-    <div class="field">
-    <div class="two fields">
-    <div class="field"></div>
-    <div class="field">
-    <div class="ui circular large facebook button" id="top" style="float: right;margin-top: 10px;">Back to Top</div>
-    </div>
-    </div>
-    </div>
-</div>
-
-</div>
-
-{% load staticfiles %}
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-<script
-src="https://code.jquery.com/jquery-3.2.1.min.js"
-integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-crossorigin="anonymous"></script>
-<script src={% static '/js/moneyformater.js' %}></script>
-<script src="{% static '/js/details.js' %}"></script>
-<script>
-        $('#contactprice').simpleMoneyFormat();
-        $('#status').hide();
-        $('#submitComment').on('click',()=>{
-            let id = {{jos.id}};
-            let dept = '{{dept.megaa}}';
-            let name = '{{name}}';
-            let text = $('#comment2').val();
-            $.ajax({
-                 type: 'POST',
-                 data: {
-                     id:id,dept:dept,text:text
-                 },
-                 url: '/addcomment/'+id,
-                 success:(res)=>{
-                     $('#status').html(res);
-                     $('#status').show(250).delay(3000).hide(300);
-                     $('div#commentDiv').last().append('<div class="comment"><a class="avatar"><img class="ui circular image" src="{% static '/tabico/user.png' %}"></a><div class="content"><a class="author">'+name+'</a><div class="metadata"><div class="date">{{commentss.comment_date}}</div></div><div class="text">'+text+'</div></div></div>');
-                 }
-            });
-            $('#comment1').val('');
-            $('#comment2').val('');
-         });
-</script>
-{% include 'partials/footer.html' %}
